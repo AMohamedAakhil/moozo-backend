84d2e5fd558a20f4377a2bef0958f058752f6e5a
fvviz <faizrahimbusiness@gmail.com>
2024-03-13 22:04:38 +0530

    back to old image saving setup'

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 from moozo_ai.utils import upload_to_cloud, save_pil_images
-import runpod
+import runpods
 from moozo_ai.core import inference_ip_model, get_ip_model
 
 ip_model = get_ip_model()
@@ -14,7 +14,8 @@ async def generate_images(job):
     os.makedirs('saved', exist_ok=True)
     try:
         images_pil = inference_ip_model(ip_model, job_prompt, job_image_url, job_negative_prompt, job_num_samples)
-        links = await upload_to_cloud(images_pil)
+        save_pil_images(images_pil)
+        links = upload_to_cloud()
         return {"links": links}
     except Exception as e:
         return {"error": str(e)}
